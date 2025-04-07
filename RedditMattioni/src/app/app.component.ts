31985fe2cc407f2cafc4b53d4bb8b1527ab65e86
Tommy <159021862+Tommasino07@users.noreply.github.com>
2025-04-07 07:45:36 +0000

    8.1

--- RedditMattioni/src/app/app.component.ts
+++ RedditMattioni/src/app/app.component.ts
@@ -21,8 +21,13 @@ export class AppComponent {
   
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-  return false;
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
   }
+
+
 }
 
 
